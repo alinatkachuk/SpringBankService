8b5ac90e20b5a49158ca5bacbb9b658a1c812b56
alinatkachuk <atkachuk2790@gmail.com>
2021-03-23 20:48:21 +0300

    changes6

---
+++ users_db.sql
@@ -0,0 +1,12 @@
+-- Database: users_db
+
+-- DROP DATABASE users_db;
+
+CREATE DATABASE users_db
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
